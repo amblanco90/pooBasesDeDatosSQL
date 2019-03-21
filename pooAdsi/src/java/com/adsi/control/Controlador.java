3dd5ccf19cfe8303f1f0386af80c817f44ebd19f
Ricardo Enrique Cantillo Carrillo <48037613+recantilloSena@users.noreply.github.com>
2019-03-20 21:27:57 -0500

    Add files via upload

--- pooAdsi/src/java/com/adsi/control/Controlador.java
+++ pooAdsi/src/java/com/adsi/control/Controlador.java
@@ -6,6 +6,7 @@
 package com.adsi.control;
 
 import com.adsi.modelo.Aprendiz;
+import com.adsi.modelo.Curso;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -17,7 +18,7 @@ import java.util.LinkedList;
 
 public class Controlador extends Conexion {
     
-public LinkedList listarAprendices( ) throws SQLException {
+public LinkedList findAllAprendices( ) throws SQLException {
         if (!hayConexion()) {
             throw new SQLException(" No existe conexion abierta ");
         }
@@ -28,9 +29,11 @@ public LinkedList listarAprendices( ) throws SQLException {
         LinkedList lista = new LinkedList();
         try {
             String query;
-            query = "select * from aprendices where fk_curso=1";    //Definir la consulta
+            query = "select * from aprendices  ";    //Definir la consulta
             pst = con.prepareStatement(query);                      //Prepararla
+                  
             rst = pst.executeQuery();                               //Ejecutarla 
+            
             
             while (rst.next()) {
                 lista.add(Aprendiz.load(rst));     //Recorre el RS y llena una lista
@@ -50,7 +53,116 @@ public LinkedList listarAprendices( ) throws SQLException {
         return lista;                            //Retorna la lista llena
     }
 
+
+public LinkedList findAprendicesCurso(int p_curso ) throws SQLException {
+        if (!hayConexion()) {
+            throw new SQLException(" No existe conexion abierta ");
+        }
         
+        
+        PreparedStatement pst = null;
+        ResultSet rst = null;
+        LinkedList lista = new LinkedList();
+        try {
+            String query;
+            query = "select * from aprendices where fk_curso = ?  ";    //Definir la consulta
+            pst = con.prepareStatement(query);                      //Prepararla
+            pst.setInt(1, p_curso);
+            
+            rst = pst.executeQuery();                               //Ejecutarla 
+            
+            
+            while (rst.next()) {
+                lista.add(Aprendiz.load(rst));     //Recorre el RS y llena una lista
+            }
+            System.out.println("El Query " + query );
+            
+        }  finally {
+            if (pst != null) {
+                pst.close();
+                pst = null;
+            }
+            if (rst != null) {
+                rst.close();
+                rst = null;
+            }
+        }
+        return lista;                            //Retorna la lista llena
+    }
+
+public LinkedList findCursoByIdCurso(int p_curso ) throws SQLException {
+        if (!hayConexion()) {
+            throw new SQLException(" No existe conexion abierta ");
+        }
+        
+        
+        PreparedStatement pst = null;
+        ResultSet rst = null;
+        LinkedList lista = new LinkedList();
+        try {
+            String query;
+            query = "select * from cursos where id_cursos = ? ";    //Definir la consulta
+            pst = con.prepareStatement(query);                      //Prepararla
+            pst.setInt(1, p_curso);
+            
+            rst = pst.executeQuery();                               //Ejecutarla 
+            
+            
+            while (rst.next()) {
+                lista.add(Curso.load(rst));     //Recorre el RS y llena una lista
+            }
+            System.out.println("El Query " + query );
+            
+        }  finally {
+            if (pst != null) {
+                pst.close();
+                pst = null;
+            }
+            if (rst != null) {
+                rst.close();
+                rst = null;
+            }
+        }
+        return lista;                            //Retorna la lista llena
+    }
+ //Select * from  ;
+  
+public LinkedList findAprendicesCursoEdad(int p_curso,int edad ) throws SQLException {
+        if (!hayConexion()) {
+            throw new SQLException(" No existe conexion abierta ");
+        }
+        
+        
+        PreparedStatement pst = null;
+        ResultSet rst = null;
+        LinkedList lista = new LinkedList();
+        try {
+            String query;
+            query = "Select * from aprendices a where a.fk_curso = ? and edad> ? ";    //Definir la consulta
+            pst = con.prepareStatement(query);                      //Prepararla
+            pst.setInt(1, p_curso);
+            pst.setInt(2, edad);
+            
+            rst = pst.executeQuery();                               //Ejecutarla 
+            
+            
+            while (rst.next()) {
+                lista.add(Aprendiz.load(rst));     //Recorre el RS y llena una lista
+            }
+            System.out.println("El Query " + query );
+            
+        }  finally {
+            if (pst != null) {
+                pst.close();
+                pst = null;
+            }
+            if (rst != null) {
+                rst.close();
+                rst = null;
+            }
+        }
+        return lista;                            //Retorna la lista llena
+    }
    
     
 }
